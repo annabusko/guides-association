511cd166281c97f9fa3231c4c1140a4e6117beb8
Anna Busko <118979722+abusko-idt@users.noreply.github.com>
2026-05-13 21:00:48 +0200

    refactoring: change slider

--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -14,7 +14,7 @@ function MenuItems() {
                 as={Link} to='/'
                 name='home'
                 active={activeItem === '/' || activeItem === 'home'}
-                onClick={e => setActiveItem(e.target.name)}>
+                onClick={() => setActiveItem('home')}>
                 {t('main_menu')}
             </Menu.Item>
 
@@ -22,16 +22,16 @@ function MenuItems() {
                 <Dropdown.Menu direction='left'>
                     <Dropdown.Item text={t('menu_history')} as={Link} to='/history' value="history"
                         active={activeItem === '/history' || activeItem === 'history'}
-                        onClick={e => setActiveItem(e.target.value)} />
+                        onClick={() => setActiveItem('history')} />
                     <Dropdown.Item text={t('menu_rules')} as={Link} to='/rules' value="rules"
                         active={activeItem === '/rules' || activeItem === 'rules'}
-                        onClick={e => setActiveItem(e.target.value)} />
+                        onClick={() => setActiveItem('rules')} />
                     <Dropdown.Item text={t('menu_board')} as={Link} to='/board' value="board"
                         active={activeItem === '/board' || activeItem === 'board'}
-                        onClick={e => setActiveItem(e.target.value)} />
+                        onClick={() => setActiveItem('board')} />
                     <Dropdown.Item text={t('menu_how_join')} as={Link} to='/join' value="join"
                         active={activeItem === '/join' || activeItem === 'join'}
-                        onClick={e => setActiveItem(e.target.value)} />
+                        onClick={() => setActiveItem('join')} />
 
                 </Dropdown.Menu>
             </Dropdown>
@@ -40,13 +40,13 @@ function MenuItems() {
                 <Dropdown.Menu direction='left'>
                     <Dropdown.Item text={t('menu_guide-main')} as={Link} to='/guide-main' value="guide-main"
                         active={activeItem === '/guide-main' || activeItem === 'guide-main'}
-                        onClick={e => setActiveItem(e.target.value)} />
+                        onClick={() => setActiveItem('guide-main')} />
                     <Dropdown.Item text={t('menu_guide-search')} as={Link} to='/guide1-search' value="guide1-search"
                         active={activeItem === '/guide1-search' || activeItem === 'guide1-search'}
-                        onClick={e => setActiveItem(e.target.value)} />
+                        onClick={() => setActiveItem('guide1-search')} />
                     <Dropdown.Item text={t('menu_guide2-search')} as={Link} to='/guide2-search' value="guide2-search"
                         active={activeItem === '/guide2-search' || activeItem === 'guide2-search'}
-                        onClick={e => setActiveItem(e.target.value)} />
+                        onClick={() => setActiveItem('guide2-search')} />
                 </Dropdown.Menu>
             </Dropdown>
 
@@ -54,13 +54,13 @@ function MenuItems() {
                 <Dropdown.Menu direction='left'>
                     <Dropdown.Item text={t('menu_certification')} as={Link} to='/certification' value="certification"
                         active={activeItem === '/certification' || activeItem === 'certification'}
-                        onClick={e => setActiveItem(e.target.value)} />
+                        onClick={() => setActiveItem('certification')} />
                     <Dropdown.Item text={t('menu_professional-development')} as={Link} to='/professional-development' value="professional-development"
                         active={activeItem === '/professional-development' || activeItem === 'professional-development'}
-                        onClick={e => setActiveItem(e.target.value)} />
+                        onClick={() => setActiveItem('professional-development')} />
                     <Dropdown.Item text={t('menu_seminars')} as={Link} to='/seminars' value="seminars"
                         active={activeItem === '/seminars' || activeItem === 'seminars'}
-                        onClick={e => setActiveItem(e.target.value)} />
+                        onClick={() => setActiveItem('seminars')} />
                 </Dropdown.Menu>
             </Dropdown>
 
@@ -69,13 +69,13 @@ function MenuItems() {
                 as={Link} to='/news'
                 name='news'
                 active={activeItem === '/news' || activeItem === 'news'}
-                onClick={e => setActiveItem(e.target.name)}>
+                onClick={() => setActiveItem('news')}>
                 {t('menu_news')}</Menu.Item>
             <Menu.Item
                 as={Link} to='/contact'
                 name='contact'
                 active={activeItem === '/contact' || activeItem === 'contact'}
-                onClick={e => setActiveItem(e.target.name)}>
+                onClick={() => setActiveItem('contact')}>
                 {t('menu_contact')}</Menu.Item>
         </Fragment>
     );
